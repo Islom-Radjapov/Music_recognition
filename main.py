8d0021af2037387a01a01031f73f23ce82d3f888
Islom-Radjapov <radjapov_islom@mail.com>
2023-01-06 16:46:31 +0500

    spotipy

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def main():
 	# print(out['track']['subtitle'])   # music aftor
 	# print(out['track']['url'])        # music url in shazam
 	# print(out['track']['myshazam']['apple']['actions'][0]['uri'])  # music url in apple music
-	# print(out['track']['sections'][1]['text'])  # music text
+	print(out['track']['sections'][1]['text'])  # music text
 	# print(out['track']['hub']['actions'][1]['uri'])   # music downloads 30 seconds
 
 
